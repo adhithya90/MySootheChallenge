be264ff015d81d9d637e50a67515af0657142fed
adhithya90 <adhithya.ramakumar@gmail.com>
2021-07-31 15:51:01 -0700

    First commit

---
+++ app/src/main/java/com/example/mysoothechallenge/ui/theme/Color.kt
@@ -0,0 +1,31 @@
+package com.example.mysoothechallenge.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//Gray
+val Gray900 = Color(0xFF333333)
+val Gray800 = Color(0xCC333333)
+
+//Rust
+val Rust600 = Color(0xFF886363)
+val Rust300 = Color(0xFFE1AFAF)
+
+//White
+val White850 = Color(0xD9FFFFFF)
+val White = Color(0xFFFFFFFF)
+val White150 = Color(0x26FFFFFF)
+val White800 = Color(0xCCFFFFFF)
+
+//Taupe
+val Taupe100 = Color(0xFFF0EAE2)
+val Taupe800 = Color(0xff655454)
+
+
+
+
+
+
+
+
+
+
